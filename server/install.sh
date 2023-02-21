c0fea3c9079b3b4d833f76d7d102dd19c4fa74ad
Torben Giesselmann <tsgiesselmann@gmail.com>
2023-02-21 15:33:16 -0800

    WIP

--- server/install.sh
+++ server/install.sh
@@ -1,5 +1,5 @@
 #!/bin/ksh
-
+# TODO: use this instead? #!/usr/bin/env ksh
 echo -n "backup0 disk id: "
 read backup0_disk_id
 echo -n "backup1 disk id: "
@@ -38,27 +38,34 @@ if [ "`echo $proceed | tr "[:upper:]" "[:lower:]"`" != "y" ]; then
 fi
 
 set -e
-install -C -m 0600 -D etc/pragnastic/backup_repo.pw                 /etc/pragnastic/backup_repo.pw
-install -C -m 0640 -D etc/pragnastic/backup.exclude                 /etc/pragnastic/backup.exclude
-install -C -m 0600 -D etc/pragnastic/data_softraid.pw               /etc/pragnastic/data_softraid.pw
-install -C -m 0640 -D etc/pragnastic/pragnastic.conf                /etc/pragnastic/pragnastic.conf
-install -m 0644 -D etc/pragnastic/pragnastic.subr                   /etc/pragnastic/pragnastic.subr
-install -m 0754 usr/local/libexec/pragnastic-backup                 /usr/local/libexec/pragnastic-backup
-install -m 0754 usr/local/libexec/pragnastic-mount                  /usr/local/libexec/pragnastic-mount
-install -m 0754 usr/local/libexec/pragnastic-show                   /usr/local/libexec/pragnastic-show
-install -m 0754 usr/local/libexec/pragnastic-raidcheck              /usr/local/libexec/pragnastic-raidcheck
-install -m 0754 usr/local/libexec/pragnastic-unmount                /usr/local/libexec/pragnastic-unmount
-install -m 0754 usr/local/sbin/pragnastic                           /usr/local/sbin/pragnastic
-install -o root -g wheel -d                                         /vol
-install -o root -g wheel -d                                         /vol/backup0
-install -o root -g wheel -d                                         /vol/backup1
-install -o root -g wheel -d                                         /vol/data
-[ ! -e /var/log/pragnastic ] && install -m 0664 /dev/null           /var/log/pragnastic
-sed -i s/alice@example.com/$notification_recipient/ /etc/pragnastic/pragnastic.conf
-sed -i s/your_backup0_disk_id/$backup0_disk_id/ /etc/pragnastic/pragnastic.conf
-sed -i s/your_backup1_disk_id/$backup1_disk_id/ /etc/pragnastic/pragnastic.conf
-sed -i s/your_data0_disk_id/$data0_disk_id/ /etc/pragnastic/pragnastic.conf
-sed -i s/your_data1_disk_id/$data1_disk_id/ /etc/pragnastic/pragnastic.conf
+
+install -m 0755 -d /etc/pragnastic
+install -m 0600 etc/pragnastic/backup_repo.pw etc/pragnastic/data_pool.pw /etc/pragnastic
+install -m 0640 etc/pragnastic/backup.exclude etc/pragnastic/pragnastic.conf /etc/pragnastic
+install -m 0644 etc/pragnastic/pragnastic.subr /etc/pragnastic
+
+install -m 0754 usr/local/libexec/pragnastic-backup /usr/local/libexec
+install -m 0754 usr/local/libexec/pragnastic-mount /usr/local/libexec/pragnastic-mount
+install -m 0754 usr/local/libexec/pragnastic-show /usr/local/libexec/pragnastic-show
+# install -m 0754 usr/local/libexec/pragnastic-raidcheck /usr/local/libexec/pragnastic-raidcheck
+install -m 0754 usr/local/libexec/pragnastic-unmount /usr/local/libexec/pragnastic-unmount
+
+install -m 0754 usr/local/sbin/pragnastic /usr/local/sbin/pragnastic
+
+install -o root -g wheel -d /vol
+install -o root -g wheel -d /vol/backup0
+install -o root -g wheel -d /vol/backup1
+install -o root -g wheel -d /vol/data
+
+[ ! -e /var/log/pragnastic ] && install -m 0664 /dev/null /var/log/pragnastic
+
+sed -i "" s/alice@example.com/$notification_recipient/ /etc/pragnastic/pragnastic.conf
+# sed -i "" s/your_backup0_disk_id/$backup0_disk_id/ /etc/pragnastic/pragnastic.conf
+# sed -i "" s/your_backup1_disk_id/$backup1_disk_id/ /etc/pragnastic/pragnastic.conf
+# sed -i "" s/your_data0_disk_id/$data0_disk_id/ /etc/pragnastic/pragnastic.conf
+# sed -i "" s/your_data1_disk_id/$data1_disk_id/ /etc/pragnastic/pragnastic.conf
+
 echo $backup_pw >/etc/pragnastic/backup_repo.pw
-echo $data_pw"\n" >/etc/pragnastic/data_softraid.pw
+echo $data_pw"\n" >/etc/pragnastic/data_pool.pw
+
 echo "done"
