fd2b9628da04831e31b42c349a8f91f6942c8aba
Torben Giesselmann <tsgiesselmann@gmail.com>
2023-02-23 22:39:01 -0800

    WIP

--- client/windows/pragnastic-conf.bat
+++ client/windows/pragnastic-conf.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
 
 SET NETDRIVE_LOCAL=N:
-SET NETDRIVE_REMOTE=\\sshfs.kr\alice@example.com/vol/data/alice/netdrive
+SET NETDRIVE_REMOTE=\\sshfs.kr\alice@example.com/vol/storage/data/alice/netdrive
 
 SET SHAREDDRIVE_LOCAL=S:
-SET SHAREDDRIVE_REMOTE=\\sshfs.kr\alice@example.com/vol/data/shared
+SET SHAREDDRIVE_REMOTE=\\sshfs.kr\alice@example.com/vol/storage/data/shared
 
 SET UNISON_EXECUTABLE=C:\"Program Files"\Unison\bin\unison.exe
 SET UNISON_PROFILE=alice
