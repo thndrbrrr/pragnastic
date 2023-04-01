98fca0c0a992a829d33175999e7e6491d4eda199
Torben Giesselmann <tsgiesselmann@gmail.com>
2023-03-31 19:05:08 -0700

    Cleanup FreeBSD install script

--- server/freebsd/install.sh
+++ server/freebsd/install.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env oksh
-# TODO: use this instead? #!/usr/bin/env ksh
-# echo -n "backup0 disk id: "
-# read backup0_disk_id
-# echo -n "backup1 disk id: "
-# read backup1_disk_id
+
 echo -n "backup repo password (input is not shown): "
 stty -echo 2>/dev/null
 read -s backup_pw
 stty echo 2>/dev/null
 echo
-# echo -n "data0 disk id: "
-# read data0_disk_id
-# echo -n "data1 disk id: "
-# read data1_disk_id
 echo -n "data pool password (input is not shown): "
 stty -echo 2>/dev/null
 read -s data_pw
@@ -22,11 +14,7 @@ echo -n "notification recipient's email (leave empty to disable): "
 read notification_recipient
 echo 
 echo "Installing PragNAStic with this config:"
-# echo "  backup0 disk id: $backup0_disk_id"
-# echo "  backup1 disk id: $backup1_disk_id"
 echo "  backup repo password: ****"
-# echo "  data0 disk id: $data0_disk_id"
-# echo "  data1 disk id: $data1_disk_id"
 echo "  data RAID password: ****"
 echo "  notification recipient: $notification_recipient"
 echo -n "Proceed? [y/N] "
@@ -52,18 +40,9 @@ install -m 0754 usr/local/libexec/pragnastic-unmount /usr/local/libexec/pragnast
 
 install -m 0754 usr/local/sbin/pragnastic /usr/local/sbin/pragnastic
 
-# install -o root -g wheel -d /vol
-# install -o root -g wheel -d /vol/backup0
-# install -o root -g wheel -d /vol/backup1
-# install -o root -g wheel -d /vol/data
-
 [ ! -e /var/log/pragnastic ] && install -m 0664 /dev/null /var/log/pragnastic
 
 sed -i "" s/alice@example.com/$notification_recipient/ /etc/pragnastic/pragnastic.conf
-# sed -i "" s/your_backup0_disk_id/$backup0_disk_id/ /etc/pragnastic/pragnastic.conf
-# sed -i "" s/your_backup1_disk_id/$backup1_disk_id/ /etc/pragnastic/pragnastic.conf
-# sed -i "" s/your_data0_disk_id/$data0_disk_id/ /etc/pragnastic/pragnastic.conf
-# sed -i "" s/your_data1_disk_id/$data1_disk_id/ /etc/pragnastic/pragnastic.conf
 
 echo $backup_pw >/etc/pragnastic/backup_repo.pw
 echo $data_pw >/etc/pragnastic/storage_pool.pw
